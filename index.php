110f9edfd083eedb0fa5a33001cf339b91c44b19
jessicafd28 <jfeliced@gmail.com>
2023-10-25 17:45:19 +0700

    multi step form

--- index.php
+++ index.php
@@ -54,25 +54,6 @@ require "ok2.php";
             color: gray;
         }
 
-        .search_input {
-            width: 100%;
-            border-radius: 10px;
-            border: none;
-            line-height: initial;
-            padding-top: 4px;
-            padding-bottom: 4px;
-            color: #f19159;
-        }
-
-        .search_input:focus-visible {
-            outline: none;
-        }
-
-        input.search_input::placeholder {
-            color: #cfd2ec;
-            vertical-align: middle;
-        }
-
         .navbar-toggler {
             border: 3px solid #f19159;
         }
@@ -85,12 +66,235 @@ require "ok2.php";
             box-shadow: none;
         }
 
-        .required-label::after {
-            content: " *";
-            color: red;
-            /* Red color */
-            margin-left: 5px;
-            /* Adjust the space as needed */
+        .shadowed-square {
+            margin-top: 50px;
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            padding: 10px;
+            box-shadow: 0 0 5px rgba(0, 0, 0, 0.2);
+        }
+
+        .container {
+            max-width: 800px;
+        }
+
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        body {
+            font-family: "Inter", sans-serif;
+        }
+
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+        .formbold-main-wrapper {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 48px;
+        }
+
+        .formbold-form-wrapper {
+            margin: 0 auto;
+            max-width: 550px;
+            width: 100%;
+            background: white;
+        }
+
+        .formbold-steps {
+            padding-bottom: 18px;
+            margin-bottom: 35px;
+            border-bottom: 1px solid #DDE3EC;
+        }
+
+        .formbold-steps ul {
+            padding: 0;
+            margin: 0;
+            list-style: none;
+            display: flex;
+            gap: 40px;
+        }
+
+        .formbold-steps li {
+            display: flex;
+            align-items: center;
+            gap: 14px;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 24px;
+            color: #536387;
+        }
+
+        .formbold-steps li span {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background: #DDE3EC;
+            border-radius: 50%;
+            width: 36px;
+            height: 36px;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 24px;
+            color: #536387;
+        }
+
+        .formbold-steps li.active {
+            color: #07074D;
+            ;
+        }
+
+        .formbold-steps li.active span {
+            background: #198754;
+            color: #FFFFFF;
+        }
+
+        .formbold-input-flex {
+            display: flex;
+            gap: 20px;
+            margin-bottom: 22px;
+        }
+
+        .formbold-input-flex>div {
+            width: 50%;
+        }
+
+        .formbold-form-input {
+            width: 100%;
+            padding: 13px 22px;
+            border-radius: 5px;
+            border: 1px solid #DDE3EC;
+            background: #FFFFFF;
+            font-weight: 500;
+            font-size: 16px;
+            color: #536387;
+            outline: none;
+            resize: none;
+        }
+
+        .formbold-form-input:focus {
+            border-color: #198754;
+            box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
+        }
+
+        .formbold-form-label {
+            color: #07074D;
+            font-weight: 500;
+            font-size: 14px;
+            line-height: 24px;
+            display: block;
+            margin-bottom: 10px;
+        }
+
+        .formbold-form-confirm {
+            border-bottom: 1px solid #DDE3EC;
+            padding-bottom: 35px;
+        }
+
+        .formbold-form-confirm p {
+            font-size: 16px;
+            line-height: 24px;
+            color: #536387;
+            margin-bottom: 22px;
+            width: 75%;
+        }
+
+        .formbold-form-confirm>div {
+            display: flex;
+            gap: 15px;
+        }
+
+        .formbold-confirm-btn {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+            background: #FFFFFF;
+            border: 0.5px solid #DDE3EC;
+            border-radius: 5px;
+            font-size: 16px;
+            line-height: 24px;
+            color: #536387;
+            cursor: pointer;
+            padding: 10px 20px;
+            transition: all .3s ease-in-out;
+        }
+
+        .formbold-confirm-btn {
+            box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.12);
+        }
+
+        .formbold-confirm-btn.active {
+            background: #198754;
+            color: #FFFFFF;
+        }
+
+        .formbold-form-step-1,
+        .formbold-form-step-2,
+        .formbold-form-step-3 {
+            display: none;
+        }
+
+        .formbold-form-step-1.active,
+        .formbold-form-step-2.active,
+        .formbold-form-step-3.active {
+            display: block;
+        }
+
+        .formbold-form-btn-wrapper {
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            gap: 25px;
+            margin-top: 25px;
+        }
+
+        .formbold-back-btn {
+            cursor: pointer;
+            background: #FFFFFF;
+            border: none;
+            color: #07074D;
+            font-weight: 500;
+            font-size: 16px;
+            line-height: 24px;
+            display: none;
+        }
+
+        .formbold-back-btn.active {
+            display: block;
+        }
+
+        .formbold-btn {
+            display: flex;
+            align-items: center;
+            gap: 5px;
+            font-size: 16px;
+            border-radius: 5px;
+            padding: 10px 25px;
+            border: none;
+            font-weight: 500;
+            background-color: #198754;
+            color: white;
+            cursor: pointer;
+        }
+
+        .formbold-btn:hover {
+            box-shadow: 0px 3px 8px rgba(0, 0, 0, 0.05);
+        }
+
+        .formbold-form-label {
+            font-size: 18px;
+        }
+
+        .form-check-label {
+            font-size: 18px;
+        }
+
+        .form-check-input {
+            transform: scale(1.5);
+            margin-right: 10px;
         }
     </style>
 </head>
@@ -118,17 +322,133 @@ require "ok2.php";
         </div>
     </nav>
 
-    <div class="container p-5">
+    <div class="formbold-main-wrapper">
+        <!-- Author: FormBold Team -->
+        <!-- Learn More: https://formbold.com -->
+        <div class="formbold-form-wrapper">
+            <form action="https://formbold.com/s/FORM_ID" method="POST">
+                <div class="formbold-steps">
+                    <ul>
+                        <li class="formbold-step-menu1 active">
+                            <span>1</span>
+                            Simulation Information
+                        </li>
+                        <li class="formbold-step-menu2">
+                            <span>2</span>
+                            Truck Percentage
+                        </li>
+                        <li class="formbold-step-menu3">
+                            <span>3</span>
+                            Warehouse Specificity
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="formbold-form-step-1 active">
+                    <div class="formbold-input-flex">
+                        <div>
+                            <label for="SimulationName" class="formbold-form-label"> Simulation Name <span style="color: red;">*</span></label>
+                            <input type="text" name="simulationName" placeholder="Simulation One" id="simulationName" class="formbold-form-input" required />
+                        </div>
+                        <div>
+                            <label for="truckContent" class="formbold-form-label"> Truck Content <span style="color: red;">*</span> </label>
+                            <input type="text" name="truckContent" placeholder="100 (kg)" id="truckContent" class="formbold-form-input" required />
+                        </div>
+                    </div>
+
+                    <div class="formbold-input-flex">
+                        <div>
+                            <label for="areaAmount" class="formbold-form-label"> Area Amount <span style="color: red;">*</span> </label>
+                            <input type="number" name="areaAmount" placeholder="4" id="areaAmount" class="formbold-form-input" required />
+                        </div>
+                        <div>
+                            <label for="totalTruck" class="formbold-form-label"> Total Truck <span style="color: red;">*</span> </label>
+                            <input type="number" name="totalTruck" placeholder="4" id="totalTruck" class="formbold-form-input" required />
+                        </div>
+                    </div>
+                    <div class="formbold-input-flex">
+                        <div>
+                            <label for="loadingTime" class="formbold-form-label"> Loading Time <span style="color: red;">*</span> </label>
+                            <input type="number" name="loadingTime" placeholder="30 minutes" id="loadingTime" class="formbold-form-input" required />
+                        </div>
+                        <div>
+                            <label for="duration" class="formbold-form-label"> Duration <span style="color: red;">*</span></label>
+                            <input type="duration" name="duration" id="duration" placeholder="24 hours" class="formbold-form-input" required />
+                        </div>
+                    </div>
+                    <div>
+                        <div>
+                            <label for="warehouseInventory" class="formbold-form-label"> Warehouse Inventory <span style="color: red;">*</span> </label>
+                            <input type="number" name="warehouseInventory" placeholder="4" id="warehouseInventory" class="formbold-form-input" required />
+                        </div>
+                    </div>
+                </div>
+
+                <div class="formbold-form-step-2">
+                    <div>
+                        <label for="setPercentage" class="formbold-form-label"> Set Percentage </label>
+                        <input type="number" name="setPercentage" id="setPercentage" placeholder="60%" class="formbold-form-input"></input>
+                    </div>
+                </div>
+
+                <div class="formbold-form-step-3">
+                    <div>
+                        <label class="formbold-form-label"> Warehouse Specificity </label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="warehouseSpecifity" id="warehouseSpecifity1" value="Area 1">
+                            <label class="form-check-label" for="warehouseSpecifity1">Area 1</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="warehouseSpecifity" id="warehouseSpecifity2" value="Area 2">
+                            <label class="form-check-label" for="warehouseSpecifity2">Area 2</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="warehouseSpecifity" id="warehouseSpecifity3" value="Area 3">
+                            <label class="form-check-label" for="warehouseSpecifity3">Area 3</label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="formbold-form-btn-wrapper">
+                    <button class="formbold-back-btn">
+                        Back
+                    </button>
+
+                    <button class="formbold-btn">
+                        Next Step
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clip-path="url(#clip0_1675_1807)">
+                                <path d="M10.7814 7.33312L7.20541 3.75712L8.14808 2.81445L13.3334 7.99979L8.14808 13.1851L7.20541 12.2425L10.7814 8.66645H2.66675V7.33312H10.7814Z" fill="white" />
+                            </g>
+                            <defs>
+                                <clipPath id="clip0_1675_1807">
+                                    <rect width="16" height="16" fill="white" />
+                                </clipPath>
+                            </defs>
+                        </svg>
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+
+
+    <div class="container p-4 shadowed-square">
         <form id="addDataForm" method="POST">
-            <h1>Input Data</h1>
+            <h2>Input form</h2>
+            <h6>Plese fill out the following information for the simulation</h6>
+            <br>
             <div class="mb-3">
                 <label for="inputNamaSimul" class="form-label">Simulation Name <span style="color: red;">*</span></label>
                 <input name="namaSimulasi" type="text" class="form-control required-label" id="inputNamaSimul" required>
             </div>
+
             <div class="mb-3">
                 <div class="row">
                     <div class="col-md-6">
-                        <label for="inputJumlahArea" class="form-label">Area Amount<span style="color: red;">*</span></label>
+                        <label for "inputJumlahArea" class="form-label">Area Amount<span style="color: red;">*</span></label>
                         <input type="number" name="jumlahArea" class="form-control required-label" id="inputJumlahArea" required>
                     </div>
                     <div class="col-md-6">
@@ -140,7 +460,6 @@ require "ok2.php";
             </div>
 
             <div class="mb-3" id="result1">
-                <!-- Content generated by JavaScript will be displayed here -->
             </div>
 
             <div class="mb-3">
@@ -265,8 +584,8 @@ require "ok2.php";
 
         function success() {
             Swal.fire({
-                title: 'Good job!',
-                text: 'You clicked the button!',
+                title: 'Success!',
+                text: 'Data has been saved!',
                 icon: 'success',
                 timer: 1000, // 5000 milliseconds (5 seconds)
                 showConfirmButton: false, // To prevent users from closing it manually
@@ -276,14 +595,14 @@ require "ok2.php";
         }
 
         // ini buat kalo nggak berhasil masuk datanya, tolong nti yang back end buatin ya thank you
-        function failed(){
+        function failed() {
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
                 text: 'Something went wrong!',
                 timer: 1000,
             }).then(() => {
-                document.getElementById("addDataForm").submit();
+                location.reload();
             })
         }
 
@@ -298,6 +617,58 @@ require "ok2.php";
             // Redirect the user to compare.php
             window.location.href = "compare.php";
         }
+
+        const stepMenuOne = document.querySelector('.formbold-step-menu1')
+        const stepMenuTwo = document.querySelector('.formbold-step-menu2')
+        const stepMenuThree = document.querySelector('.formbold-step-menu3')
+
+        const stepOne = document.querySelector('.formbold-form-step-1')
+        const stepTwo = document.querySelector('.formbold-form-step-2')
+        const stepThree = document.querySelector('.formbold-form-step-3')
+
+        const formSubmitBtn = document.querySelector('.formbold-btn')
+        const formBackBtn = document.querySelector('.formbold-back-btn')
+
+        formSubmitBtn.addEventListener("click", function(event) {
+            event.preventDefault()
+            if (stepMenuOne.className == 'formbold-step-menu1 active') {
+                event.preventDefault()
+
+                stepMenuOne.classList.remove('active')
+                stepMenuTwo.classList.add('active')
+
+                stepOne.classList.remove('active')
+                stepTwo.classList.add('active')
+
+                formBackBtn.classList.add('active')
+                formBackBtn.addEventListener("click", function(event) {
+                    event.preventDefault()
+
+                    stepMenuOne.classList.add('active')
+                    stepMenuTwo.classList.remove('active')
+
+                    stepOne.classList.add('active')
+                    stepTwo.classList.remove('active')
+
+                    formBackBtn.classList.remove('active')
+
+                })
+
+            } else if (stepMenuTwo.className == 'formbold-step-menu2 active') {
+                event.preventDefault()
+
+                stepMenuTwo.classList.remove('active')
+                stepMenuThree.classList.add('active')
+
+                stepTwo.classList.remove('active')
+                stepThree.classList.add('active')
+
+                formBackBtn.classList.remove('active')
+                formSubmitBtn.textContent = 'Submit'
+            } else if (stepMenuThree.className == 'formbold-step-menu3 active') {
+                document.querySelector('form').submit()
+            }
+        })
     </script>
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
