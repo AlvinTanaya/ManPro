afad6ac8391dff4f7f8066c95f03df9b1f09e87c
Gilbert Lorentz <gilbertlorentz@gmail.com>
2023-11-11 23:51:35 +0700

    Ubah Simulation

--- simulation.php
+++ simulation.php
@@ -1,22 +1,38 @@
 <?php
 // Random number generation and math functions
-function getRandomInt($min, $max) {
+function getRandomInt($min, $max)
+{
     return rand($min, $max);
 }
 
+// Random number generation and math functions
+function getRandomFloat($min, $max, $decimalPlaces = 1)
+{
+    $randomValue = $min + mt_rand() / mt_getrandmax() * ($max - $min);
+    return round($randomValue, $decimalPlaces);
+}
+
+function isWithinRange($value, $lower, $upper, $tolerance)
+{
+    return $value >= ($lower - $tolerance) && $value <= ($upper + $tolerance);
+}
+
 // Class to represent Gudang
-class Gudang {
+class Gudang
+{
     private $kriteria;
     private $waitList = array();
     private $currentWaktuTunggu = 0;
     private $totalWaktuTunggu = 0;
     private $daftarTruk = array();
 
-    public function __construct($kriteria) {
+    public function __construct($kriteria)
+    {
         $this->kriteria = $kriteria;
     }
 
-    public function cekWaitList() {
+    public function cekWaitList()
+    {
         if (count($this->waitList) > 0) {
             if ($this->currentWaktuTunggu < 1) {
                 $this->loadingMuatan(array_shift($this->waitList));
@@ -24,36 +40,43 @@ class Gudang {
         }
     }
 
-    public function loadingMuatan($truk) {
+    public function loadingMuatan($truk)
+    {
         $this->daftarTruk[] = $truk->getID();
         $this->currentWaktuTunggu += $truk->getWaktuLoading();
     }
 
-    public function masukWaitList($truk) {
+    public function masukWaitList($truk)
+    {
         $this->waitList[] = $truk;
     }
 
-    public function timeTick() {
+    public function timeTick()
+    {
         if ($this->currentWaktuTunggu > 0) $this->currentWaktuTunggu -= 1;
         if ($this->totalWaktuTunggu > 0) $this->totalWaktuTunggu -= 1;
     }
 
-    public function getCurrentWaktuTunggu() {
+    public function getCurrentWaktuTunggu()
+    {
         return $this->currentWaktuTunggu;
     }
 
-    public function getTotalWaktuTunggu() {
+    public function getTotalWaktuTunggu()
+    {
         return $this->totalWaktuTunggu;
     }
 
-    public function getKriteria() {
+    public function getKriteria()
+    {
         return $this->kriteria;
     }
 
-    public function getDaftarTruk() {
+    public function getDaftarTruk()
+    {
         return $this->daftarTruk;
     }
-    
+
 
     // Add other methods and properties as needed
 }
@@ -61,15 +84,19 @@ class Gudang {
 
 
 // Class to represent Truk
-class Truk {
+class Truk
+{
     private $id;
     private $indexJarak;
     private $jarak;
     private $waktuLoading;
     private $kriteria;
     private $randomTime;
+    private $delay;
+    private $waktuSampai;
 
-    public function __construct($id, $indexJarak, $jarak, $waktuLoading, $durasiSimulasi) {
+    public function __construct($id, $indexJarak, $jarak, $waktuLoading, $durasiSimulasi)
+    {
         $this->id = $id;
         $this->indexJarak = $indexJarak;
         $this->jarak = $jarak;
@@ -77,33 +104,68 @@ class Truk {
 
         // Generate a random time within the specified range
         $this->randomTime = date("H:i:s", strtotime("00:00:00") + rand(0, $durasiSimulasi * 3600));
+        $this->delay = getRandomInt(0, 180);
+
+        $waktuSampaiInMinutes = strtotime($this->getRandomTime()) + $this->getTravelTime() * 60 + $this->getDelay() * 60;
+
+        // Format 'Waktu Sampai' to 24-hour time
+        $this->waktuSampai = date("H:i:s", $waktuSampaiInMinutes);
     }
 
-    public function getID() {
+    public function setDelay($delay)
+    {
+        $this->delay = $delay;
+    }
+
+    public function getDelay()
+    {
+        return $this->delay;
+    }
+
+    public function getWaktuSampai()
+    {
+        return $this->waktuSampai;
+    }
+
+
+    public function getID()
+    {
         return $this->id;
     }
 
-    public function getIndexJarak() {
+    public function getIndexJarak()
+    {
         return $this->indexJarak;
     }
 
-    public function getWaktuLoading() {
+    public function getWaktuLoading()
+    {
         return $this->waktuLoading;
     }
 
-    public function getJarak() {
+    public function getJarak()
+    {
         return $this->jarak;
     }
 
-    public function getKriteria() {
+    public function getKriteria()
+    {
         return $this->kriteria;
     }
 
-    public function getRandomTime() {
+    public function getRandomTime()
+    {
         return $this->randomTime;
     }
 
-    public function printInfo() {
+    public function getTravelTime()
+    {
+        // Calculate travel time based on the criteria (20 minutes for every 1 jarak)
+        return $this->jarak * 20;
+    }
+
+    public function printInfo()
+    {
         echo $this->id . " " . $this->indexJarak . " " . $this->jarak . " " . $this->waktuLoading . " " . $this->randomTime . "<br>";
     }
 }
@@ -111,10 +173,10 @@ class Truk {
 
 // Input Range Jarak
 $daftarJarak = array(
-    array(1, 4),
-    array(5, 10),
-    array(11, 16),
-    array(17, 25)
+    array(1, 5),
+    array(5, 11),
+    array(11, 17),
+    array(17, 26)
 );
 
 // Input Total Truk, Loading Time, and Percentage
@@ -185,10 +247,15 @@ $id = 1;
 // Randomize truck distances based on their range
 for ($i = 0; $i < count($jumlahTruk); $i++) {
     for ($j = 0; $j < $jumlahTruk[$i]; $j++) {
-        $daftarTruk[] = new Truk($id, $i, getRandomInt($daftarJarak[$i][0], $daftarJarak[$i][1]), $waktuLoading, $durasiSimulasi);
+        $lowerBound = $daftarJarak[$i][0];
+        $upperBound = $daftarJarak[$i][1];
+        $jarak = getRandomFloat($lowerBound, $upperBound, 1);
+
+        $daftarTruk[] = new Truk($id, $i, $jarak, $waktuLoading, $durasiSimulasi);
         $id++;
     }
 }
+
 
 // Run the simulation for the specified duration
 $durasi = 0;
@@ -211,17 +278,19 @@ while ($simulasi && $durasi < $durasiSimulasi) {
 
     // Process the arrived trucks
     for ($i = 0; $i < count($arrivalTruk); $i++) {
-        $jarak = $arrivalTruk[$i]->getIndexJarak();
+        $jarak = $arrivalTruk[$i]->getJarak();
         $waktuTunggu = $durasiSimulasi;
         $indexGudang = null;
 
         for ($j = 0; $j < count($daftarGudang); $j++) {
             $temp = $daftarGudang[$j]->getCurrentWaktuTunggu() + $daftarGudang[$j]->getTotalWaktuTunggu();
 
-            if (in_array($jarak, $daftarGudang[$j]->getKriteria()) && $temp == 0) {
-                $indexGudang = $j;
-                $waktuTunggu = 0;
-            } elseif (in_array($jarak, $daftarGudang[$j]->getKriteria())) {
+            // Check if the truck's distance falls within the range of the warehouse criteria
+            $tolerance = 0.1; // Adjust the tolerance as needed
+            $lowerBound = $daftarJarak[$j][0];
+            $upperBound = $daftarJarak[$j][1];
+
+            if (isWithinRange($jarak, $lowerBound, $upperBound, $tolerance)) {
                 $temp = $daftarGudang[$j]->getCurrentWaktuTunggu() + $daftarGudang[$j]->getTotalWaktuTunggu();
 
                 if ($waktuTunggu > $temp) {
@@ -234,11 +303,15 @@ while ($simulasi && $durasi < $durasiSimulasi) {
         if ($indexGudang === null) {
             $daftarTolak[] = $arrivalTruk[$i];
         } elseif ($indexGudang !== null && $waktuTunggu == 0) {
+            // Set the delay for each truck
+            $arrivalTruk[$i]->setDelay(getRandomInt(0, 180));
+
             $daftarGudang[$indexGudang]->loadingMuatan($arrivalTruk[$i]);
         } else {
             $daftarGudang[$indexGudang]->masukWaitList($arrivalTruk[$i]);
         }
     }
+
 
     // Advance time for Gudang and the simulation duration
     for ($i = 0; $i < count($daftarGudang); $i++) {
@@ -247,11 +320,24 @@ while ($simulasi && $durasi < $durasiSimulasi) {
     $durasi++;
 }
 
-// Print truck information
-echo "Daftar Truk:<br>";
+echo "<h2>Daftar Truk:</h2>";
+echo "<table border='1'>";
+echo "<tr><th>ID</th><th>Index Jarak</th><th>Jarak</th><th>Waktu Loading</th><th>Waktu Berangkat</th><th>Travel Time (min)</th><th>Delay (min)</th><th>Waktu Sampai</th></tr>";
+
 foreach ($daftarTruk as $truk) {
-    $truk->printInfo();
+    echo "<tr>";
+    echo "<td>" . $truk->getID() . "</td>";
+    echo "<td>" . $truk->getIndexJarak() . "</td>";
+    echo "<td>" . $truk->getJarak() . "</td>";
+    echo "<td>" . $truk->getWaktuLoading() . "</td>";
+    echo "<td>" . $truk->getRandomTime() . "</td>";
+    echo "<td>" . $truk->getTravelTime() . "</td>";
+    echo "<td>" . $truk->getDelay() . "</td>";
+    echo "<td>" . $truk->getWaktuSampai() . "</td>";  // New 'Waktu Sampai' column
+    echo "</tr>";
 }
+
+echo "</table>";
 
 // Create an instance of the Gudang class and specify the kriteria
 $gudang = new Gudang($kekhususanGudang);
